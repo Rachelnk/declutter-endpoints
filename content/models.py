3039fe18cd43d8e9f44f8ed2b874b771226a0cd2
Rachel Kiarie <rachelnnk94@gmail.com>
2022-10-22 16:38:24 +0300

    Add buyer model.

--- content/models.py
+++ content/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
-class Profile(models.Model):
+class Seller(models.Model):
   user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='User', null=True )
   username = models.CharField(max_length=60)
   name = models.CharField(max_length=30)
@@ -11,4 +11,26 @@ class Profile(models.Model):
   location= models.CharField(max_length=60, blank=False)
   contact=models.CharField(max_length=20, blank=False)
 
-  
+  def save_buyer(self):
+    self.user()
+
+  def delete_buyer(self):
+    self.delete()
+
+  # def get_posts(self):
+  #   return
+
+class Buyer(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='User', null=True )
+  username = models.CharField(max_length=60)
+  name = models.CharField(max_length=30)
+  email= models.EmailField(blank=True)
+  contact=models.CharField(max_length=20, blank=False)
+
+  def save_buyer(self):
+    self.user()
+
+  def delete_buyer(self):
+    self.delete()
+
+
