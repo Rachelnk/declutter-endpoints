904e6260a875def7db618b06ba5311a7b46a6767
Rachel Kiarie <rachelnnk94@gmail.com>
2022-10-22 16:19:45 +0300

    Create user model.

--- content/models.py
+++ content/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Profile(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='User', null=True )
+  username = models.CharField(max_length=60)
+  name = models.CharField(max_length=30)
+  email= models.EmailField(blank=True)
+  location= models.CharField(max_length=60, blank=False)
+  contact=models.CharField(max_length=20, blank=False)
+
+  
